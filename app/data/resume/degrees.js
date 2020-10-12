fad02d6901885ac0bc6ca2027b37e42a9694f0b5
loganrobertclemons <loganrobertclemons@gmail.com>
2020-10-12 10:26:58 -0500

    added skills and courses

---
+++ app/data/resume/degrees.js
@@ -0,0 +1,15 @@
+const degrees = [
+    {
+        school: 'Austin Peay State University',
+        degree: 'B.S. Computer Science',
+        link: 'https://apsu.edu',
+        year: 2019,
+    }, {
+        school: 'James Madison University',
+        degree: 'B.A. English',
+        link: 'https://jmu.edu',
+        year: 2013,
+    },
+];
+
+export default degrees;
