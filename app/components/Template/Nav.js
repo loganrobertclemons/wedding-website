5564e6093bd5c293553bdb1aa3191a6465dee9e3
loganrobertclemons <loganrobertclemons@gmail.com>
2020-10-11 19:29:07 -0500

    added md files

---
+++ app/components/Template/Nav.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import data from '../../data/contact';
+
+const Nav = () => (
+    <section id="sidebar">
+        <section id="intro">
+            <Link to="/" className="logo">
+                <img src={`${BASE_PATH}/images/me_icon.jpg`} alt="" />
+            </Link>
+            <header>
+                <h2>Michael D&apos;Angelo</h2>
+                <p><a href="mailto:loganrclemons@gmail.com">loganrclemons@gmail.com</a></p>
+            </header>
+        </section>
+
+        <section className="blurb">
+            <h2>About</h2>
+            <p>Hi, I&apos;m Logan Clemons. I am an SRE and I would like to build stuff for you.</p>
+            <ul className="actions">
+                <li>
+                    {window.location.pathname !== `${BASE_PATH}/resume` ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
+                </li>
+            </ul>
+        </section>
+
+        <section id="footer">
+            <ul className="icons">
+                {data.map((s) => (
+                    <li key={s.label}>
+                        <a href={s.link}>
+                            <FontAwesomeIcon icon={s.icon} />
+                        </a>
+                    </li>
+                ))}
+            </ul>
+            <p className="copyright">&copy; Logan Clemons <Link to="/">loganrclemons.com</Link>.</p>
+        </section>
+    </section>
+);
+
+export default Nav;
